48967cbce7cfa6d8e4210b4c86e7585cd268b141
Ganesh2992 <ganeshmangalamsinha@gmail.com>
2025-07-20 22:26:36 +0530

    intial

---
+++ hello.js
@@ -0,0 +1,6 @@
+a=5;
+b=6;
+console.log(a,b);
+
+age=20;
+console.log("My age is", age , " and i am young");
---
+++ loop.js
@@ -0,0 +1,21 @@
+// for(let i=1;i<=10;i++){
+//     console.log(i);
+//   }
+
+// let i=1;
+// while(i<=15){
+//   console.log(i);
+//   i++;
+// }
+
+// let i=1;
+// do{
+//   console.log(i);
+//   i++;
+// } while(i<=14); 
+let i,j;        
+for(i=0;i<5;i++){
+  for(j=0;j<=i;j++){
+    console.log(i)
+  }
+}  
